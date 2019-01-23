3a93fa8d558a07bd7ef906a02927aa4bfc3b6ae6
Michele Fattoruso <michele.fattoruso@gmail.com>
2019-01-23 13:51:10 -0600

    state and props for UI update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import "./App.css";
 import Person from './Person/Person';
 
 class App extends Component {
+	state = {  //property of the Component class. If values change, react will update the DOM
+		persons: [
+			{ name: 'Michele', age: 28},
+			{ name: 'Bill', age: 31},
+			{ name: 'Bob', age: 22}
+		],
+		otherState: "some other value"
+	}
+
+	switchNameHandler = () => {
+		// console.log('Was clicked!');
+		this.setState({
+			persons:[
+				{ name: 'Mike', age: 28},
+				{ name: 'Bill', age: 31},
+				{ name: 'Bob', age: 24}
+			]
+		})
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<h1> Hi, I'm a React app</h1>
 				<p>This is working</p>
-				<Person name="Michele" age="28"/>
-				<Person name="Bill" age="31">My hobby is Swimming</Person>
-				<Person name="Bob" age="22"/>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is Swimming</Person>
+				<Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
 			</div>
 		);
 		// return React.createElement('div',{className:'App'},
